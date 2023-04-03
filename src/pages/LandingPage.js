bde6518eb005a9e4156f947f6816effe0e5fff39
David Amberg <david.amberg@ymail.com>
2023-04-03 18:49:17 +0200

    cleaned up landingPage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,26 +1,108 @@
-import { useState } from "react";
-import cardImage from '../foreground.jpg'
-import { KonvaScratch } from "../components/KonvaScratch2";
+import { Stage, Layer, Line,  } from "react-konva";
+import React, { useState } from "react";
+import ForeGroundImage from "../components/ForeGroundImage";
+import BackGroundImage from "../components/BackGroundImage";
+import Text2 from "../components/Headline";
+
+const LandingPage = () => {
+  const [lines, setLines] = useState([]);
+  const isDrawing = React.useRef(true);
 
 
-function LandingPage() {
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+// eslint-disable-next-line
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
 
-  const settings = {
-    width: 640,
-    height: 480,
-    image: cardImage,
-    finishPercent: 50,
-    onComplete: () => console.log('The card is now clear!')
+  React.useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return (_) => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
+  const handleMouseMove = (e) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    setLines([...lines, { points: [pos.x, pos.y] }]);
+
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    let lastLine = lines[lines.length - 1];
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
+
+    lines.splice(lines.length - 1, 1, lastLine);
+    setLines(lines.concat());
+  };
+
+  
+  const handleTouchMove = (e) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    setLines([...lines, { points: [pos.x, pos.y] }]);
+
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    let lastLine = lines[lines.length - 1];
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
+
+    lines.splice(lines.length - 1, 1, lastLine);
+    setLines(lines.concat());
   };
 
   return (
-    <>
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMousemove={handleMouseMove}
+      onTouchMove={handleTouchMove}
 
-    <KonvaScratch />
+    >
+      <Layer className="konvaBackground">
+        <BackGroundImage
+          src={
+            window.innerWidth > 900
+              ? "./images/background-wide.jpg"
+              : "./images/background.jpg"
+          }
+        />
 
-    </>
+        <Text2 />
+      </Layer>
+
+      <Layer>
+        <ForeGroundImage
+          src={
+            window.innerWidth > 900
+              ? "./images/foreground-wide.jpg"
+              : "./images/foreground.jpg"
+          }
+        />
+
+        {lines.map((line, i) => (
+          <Line
+            key={i}
+            points={line.points}
+            stroke="#df4b26"
+            strokeWidth={window.innerWidth < 576 ? 100 : 200 }
+            tension={0.5}
+            lineCap="round"
+            lineJoin="round"
+            globalCompositeOperation="destination-out"
+          />
+        ))}
+      </Layer>
+    </Stage>
   );
-}
+};
 
-export default LandingPage;
+export default LandingPage

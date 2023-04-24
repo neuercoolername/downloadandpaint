868842f5b923855d64bb975806a9190f6b22167c
David Amberg <david.amberg@ymail.com>
2023-04-24 13:10:24 +0200

    changed lineHeight font

--- src/utilities/calcFontSizeLineHeight.js
+++ src/utilities/calcFontSizeLineHeight.js
@@ -3,7 +3,7 @@ const fontSizeLineHeight = {
     768: { fontSize: 100, lineHeight: 100 },
     1000: { fontSize: 140, lineHeight: 150 },
     1400: { fontSize: 200, lineHeight: 180 },
-    Infinity: { fontSize: 250, lineHeight: 300 },
+    Infinity: { fontSize: 260, lineHeight: 260 },
   };
 
   export default fontSizeLineHeight

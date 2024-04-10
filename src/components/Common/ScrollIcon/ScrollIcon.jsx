c5ffbe97738b528a0bb0ae78f5a056c372a26d7c
David Amberg <david.amberg@ymail.com>
2024-04-10 21:17:08 +0200

    add timeout to scoll icon

--- src/components/Common/ScrollIcon/ScrollIcon.jsx
+++ src/components/Common/ScrollIcon/ScrollIcon.jsx
@@ -1,8 +1,23 @@
 import scrollIconStyle from "./scrollIcon.module.css";
+import { useState, useEffect } from "react";
 
 export default function ScrollIcon() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 6000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className={`${scrollIconStyle.wrapper}`}>
+    <div
+      className={`${scrollIconStyle.wrapper} ${scrollIconStyle.fadeIn} ${
+        isVisible ? scrollIconStyle.visible : ""
+      }`}
+    >
       <span className={scrollIconStyle.scroll}></span>
       <span className={scrollIconStyle.text}>Scroll to start</span>
     </div>
